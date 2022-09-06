80aea6a65d5a04cfa633f50e79d573ff27161cac
huzefa bin juned <huzefaghurna@gmail.com>
2022-09-06 10:09:03 +0530

    ee

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Login" element={<Login />} />
+        <Route path="/Register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
